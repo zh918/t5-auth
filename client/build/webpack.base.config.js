2315872c662e0729c7b5bf861db17e19aea0736d
zh918 <287360367@qq.com>
2019-12-12 18:01:41 +0800

    .

--- client/build/webpack.base.config.js
+++ client/build/webpack.base.config.js
@@ -79,7 +79,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)(\?.*)?$/,
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 use: [
                     {
                       loader: 'url-loader',
