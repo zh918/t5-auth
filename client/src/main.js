1ba8b07a166e2f55ff24d5ba0c2721c3d20c3b15
zh918 <287360367@qq.com>
2019-12-26 14:43:22 +0800

    .

--- client/src/main.js
+++ client/src/main.js
@@ -1,30 +1,29 @@
-
 // 自定义样式及reset
-import './style/common.less'
-import './style/reset.less'
-import 'element-ui/lib/theme-chalk/index.css';
+import "./style/common.less";
+import "./style/reset.less";
+import "lt-element/lib/theme-chalk/index.css";
 
-import Vue from 'vue'
-import Element from 'element-ui'
-import App from './App.vue' 
-import './common/storage'
-import router from './router/index'
-import store from './store/index'
-import './config/env'
-import './common/http'
+import Vue from "vue";
+import Element from "lt-element";
+import App from "./App.vue";
+import "./common/storage";
+import router from "./router/index";
+import store from "./store/index";
+import "./config/env";
+import "./common/http";
 // import './components/index'
-import {authRouter} from './common/auth'
-import './common/util' 
-import './common/tabHelper'
+import { authRouter } from "./common/auth";
+import "./common/util";
+import "./common/tabHelper";
 
-Vue.use(Element)
+Vue.use(Element);
 
 const globalVue = new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
 
-window.globalVue = globalVue 
+window.globalVue = globalVue;
 
-console.log('环境：',process.env.NODE_ENV)
+console.log("环境：", process.env.NODE_ENV);
--- client/src/views/_layout/block/leftMenu.vue
+++ client/src/views/_layout/block/leftMenu.vue
@@ -1,89 +1,93 @@
 <template>
-    <div class="left-menu-container">
-        <el-menu default-active="1-4-1" 
-            class="el-menu-vertical-demo" 
-            @open="handleOpen" 
-            @close="handleClose" 
-            :collapse="isCollapse"
-            background-color="#304156"
-            text-color="#bfcbd9"
-            active-text-color="#ffd04b">
-            <el-submenu :index="item.id" v-for="(item,index) in menu">
-                <template slot="title">
-                    <i class="el-icon-setting"></i>
-                    <span slot="title">{{item.name}}</span>
-                </template>
-                <el-menu-item :index="m.id" @click="handleChooseMenu(m.path)" v-for="(m,n) in item.children">{{m.name}}</el-menu-item>
-            </el-submenu> 
-             
-        </el-menu>
-    </div>
+  <div class="left-menu-container">
+    <el-menu
+      default-active="1-4-1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+      background-color="#304156"
+      text-color="#bfcbd9"
+      active-text-color="#ffd04b"
+    >
+      <el-submenu :index="item.id" v-for="(item, index) in menu">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span slot="title">{{ item.name }}</span>
+        </template>
+        <el-menu-item
+          :index="m.id"
+          @click="handleChooseMenu(m.path)"
+          v-for="(m, n) in item.children"
+          >{{ m.name }}</el-menu-item
+        >
+      </el-submenu>
+    </el-menu>
+  </div>
 </template>
 
 <script>
-import shortid from 'shortid' 
-import api from '@/services/commonLogic'
+import shortid from "shortid";
+import api from "@/services/commonLogic";
 
 export default {
-    name:'leftMenu',
-    props:['isCollapse'], 
-    data() {
-        return {
-            menu:[
-                {
-                    id:shortid.generate(),
-                    name:'供应商',
-                    children:[
-                        {id:shortid.generate(),name:'供应商管理1',path:'/list'},
-                        {id:shortid.generate(),name:'供应商管理2',path:'/demo'},
-                    ]
-                },
-                {
-                    id:shortid.generate(),
-                    name:'供应商2',
-                    children:[
-                        {id:shortid.generate(),name:'供应商管理1',path:'/list'},
-                        {id:shortid.generate(),name:'供应商管理2',path:'/demo'},
-                    ]
-                }
-            ]
-
+  name: "leftMenu",
+  props: ["isCollapse"],
+  data() {
+    return {
+      menu: [
+        {
+          id: shortid.generate(),
+          name: "供应商",
+          children: [
+            { id: shortid.generate(), name: "供应商管理1", path: "/list" },
+            { id: shortid.generate(), name: "供应商管理2", path: "/demo" }
+          ]
+        },
+        {
+          id: shortid.generate(),
+          name: "供应商2",
+          children: [
+            { id: shortid.generate(), name: "供应商管理1", path: "/list" },
+            { id: shortid.generate(), name: "供应商管理2", path: "/demo" }
+          ]
         }
-    }, 
-    created() {
-        this.initData();
+      ]
+    };
+  },
+  created() {
+    // this.initData();
+  },
+  methods: {
+    initData() {
+      api.getMenu().then(result => {
+        if (result.errorCode == 0) {
+          this.menu = result.data.children;
+        } else {
+          this.$message.error(result.errorMsg);
+        }
+      });
     },
-    methods: {
-        initData() {
-            api.getMenu().then(result=>{
-                if (result.errorCode == 0) {
-                    this.menu = result.data.children;
-                }
-                else {
-                    this.$message.error(result.errorMsg);
-                }
-            })
-        },
-        handleOpen(key, keyPath) {
-            console.log(key, keyPath);
-        },
-        handleClose(key, keyPath) {
-            console.log(key, keyPath);
-        },
-        handleChooseMenu(path) { 
-            console.log(path)
-            $TabHelper.open({path:path});
-        }
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleChooseMenu(path) {
+      console.log(path);
+      $TabHelper.open({ path: path });
     }
-}
+  }
+};
 </script>
 
 <style lang="less" scoped>
-    .left-menu-container {
-        width: 100%;
+.left-menu-container {
+  width: 100%;
 
-        i {
-            color: #bfcbd9;
-        }
-    }
+  i {
+    color: #bfcbd9;
+  }
+}
 </style>
